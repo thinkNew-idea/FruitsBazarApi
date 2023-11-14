b434df5f67f3dcb5539af3590a2d559be6f3ee29
Prabhash Vishwakarma <prabhashvishwakarma@Prabhashs-MacBook-Air.local>
2023-11-14 15:44:53 +0530

    changes

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const apis = require("./src/routes/apis");
 require("./src/db/connect");
 app.use(express.json());
 app.use("/api", apis);
+app.get("/",(req,res)=>{
+res.send('Apis running sucessfully !!');
+})
 const port = parseInt(process.env.PORT || 3000);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-  
+
 });
