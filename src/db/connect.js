d6162d17f77be4ba45e5c99f9889bc0ca2e8e017
Prabhash Vishwakarma <prabhashvishwakarma@Prabhashs-MacBook-Air.local>
2023-11-14 16:16:13 +0530

    changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://Prabhash:Prabhash@cluster0.dwtij59.mongodb.net/FruitsBazar')
+mongoose.connect('mongodb+srv://Prabhash:Prabhash@cluster0.dwtij59.mongodb.net/FruitsBazar',{
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 30000,
+  waitQueueTimeoutMS: 60000,
+})
   .then(() => {
     console.log("connection is successful");
   })
