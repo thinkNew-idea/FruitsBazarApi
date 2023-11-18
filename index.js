c018c8171d14dd91233b0c520e00500697f001e6
Prabhash Vishwakarma <prabhashvishwakarma@Prabhashs-MacBook-Air.local>
2023-11-18 20:55:36 +0530

    cors added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const apis = require("./src/routes/apis");
 require("./src/db/connect");
+app.use(cors()); 
 app.use(express.json());
 app.use("/api", apis);
 app.get("/",(req,res)=>{
 res.send('Apis running sucessfully !!');
 })
-const port = parseInt(process.env.PORT || 3000);
+const port = parseInt(process.env.PORT || 8000);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 
